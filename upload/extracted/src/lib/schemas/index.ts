c08b00a9675afa771863ab793397f8fa5898defb
Almuna Creative <almuna@almuna.org>
2026-06-16 23:52:10 +0000

    🔒 Remove user uploaded files from repo

--- upload/extracted/src/lib/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-export { postSchema, type PostInput } from "./posts";
-export { categorySchema, type CategoryInput } from "./categories";
-export { eventSchema, type EventInput } from "./events";
-export { changeRoleSchema, toggleStatusSchema, type ChangeRoleInput, type ToggleStatusInput } from "./users";
-export { contactSchema, type ContactInput } from "./contact";
--- upload/extracted/src/scripts/hash-password.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcryptjs";
-
-const password = process.argv[2] || "AlMunaAdmin2025!";
-
-bcrypt.hash(password, 12).then((hash) => {
-  console.log("Password:", password);
-  console.log("Hash:", hash);
-});
